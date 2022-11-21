f7dbd0ce3ee944252a56a4a859674684c2c43246
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-11-21 07:56:40 +0100

    feat: add parsing of JSON

--- netlify/functions/calendly-webhook.ts
+++ netlify/functions/calendly-webhook.ts
@@ -1,5 +1,10 @@
 export const handler = async (event) => {
-  const payload = JSON.parse(event.body)
+ let payload = {}
+ try {
+  payload = JSON.parse(event.body)
+ } catch (error) {
+  console.log("Could not parse JSON")
+ }
   console.log(payload)
   return {
     statusCode: 200,
